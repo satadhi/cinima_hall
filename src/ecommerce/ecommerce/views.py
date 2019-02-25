8d95098fec73cb18fe20922d1e54a7305beca1c5
satadhi <haldersatadhi@gmail.com>
2019-02-25 19:42:29 +0530

    bugs_fixed

--- src/ecommerce/ecommerce/views.py
+++ src/ecommerce/ecommerce/views.py
@@ -5,15 +5,20 @@ def home_page(request):
     return render(request,"home_page.html", {})
 
 #this one is having a seperate folder
-def contact_page(request):
-    context = {
-    "title": "this is contact page"
-    }
-    return render(request,"contacts/views.html", context)
 
 def about_page(request):
     return render(request,"home_page.html", {})
 
+def contact_page(request):
+    context = {
+    "title": "this is contact page"
+    }
+    if request.method == "POST":
+        #print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+    return render(request,"contacts/views.html", context)
 
 # def home_page(request):
 #     return render(request,"auth/login.html", {})
