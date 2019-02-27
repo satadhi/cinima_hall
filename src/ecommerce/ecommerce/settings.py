8578999687eec066a5ee18a31a96751f0d2a6b15
satadhi <haldersatadhi@gmail.com>
2019-02-28 03:49:59 +0530

    static_cdn_updated

--- src/ecommerce/ecommerce/settings.py
+++ src/ecommerce/ecommerce/settings.py
@@ -122,4 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static_my_proj"),
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
