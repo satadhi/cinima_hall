b3e2200ba6b714ec0abfb1aa0daa4899f2247b57
Gautam <Satadhi@Gautams-MacBook-Air.local>
2019-03-01 09:32:20 +0530

    hehe

--- src/ecommerce/products/models.py
+++ src/ecommerce/products/models.py
@@ -5,6 +5,6 @@ class Product(models.Model):
     title        = models.CharField(max_length=120)
     description  = models.TextField()
     price        = models.DecimalField(decimal_places=2,max_digits=10)
-
+    image        = models.FileField(upload_to ='product/', null=True, blank=True)
     def __str__(self):
         return self.title
