d96fb9423b55a115960fe3a2416bb6e1ac8289dd
satadhi <haldersatadhi@gmail.com>
2019-03-27 11:17:11 +0530

    slug_not_working

--- src/ecommerce/products/models.py
+++ src/ecommerce/products/models.py
@@ -1,5 +1,26 @@
+import random
+import os
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
+from ecommerce.utils import unique_slug_generator, get_filename
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    # print(instance)
+    #print(filename)
+    new_filename = random.randint(1,3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(
+            new_filename=new_filename,
+            final_filename=final_filename
+            )
 # Create your models here.
 class ProductQuerySet(models.query.QuerySet):
     def active(self):
@@ -40,10 +61,11 @@ class ProductManager(models.Manager):
 
 
 class Product(models.Model):
-    title        = models.CharField(max_length=120)
-    description  = models.TextField()
-    price        = models.DecimalField(decimal_places=2,max_digits=10)
-    image        = models.FileField(upload_to ='product/', null=True, blank=True)
+    title           = models.CharField(max_length=120)
+    slug            = models.SlugField(blank=True, unique=True)
+    description     = models.TextField()
+    price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured        = models.BooleanField(default=False)
     active          = models.BooleanField(default=True)
     timestamp       = models.DateTimeField(auto_now_add=True)
@@ -70,3 +92,8 @@ class Product(models.Model):
     def get_downloads(self):
         qs = self.productfile_set.all()
         return qs
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
--- src/ecommerce/products/views.py
+++ src/ecommerce/products/views.py
@@ -29,15 +29,15 @@ def product_detail_view(request,pk=None , *args, **kwargs):
     }
     return render(request, "products/detail.html",context)
 
-class ProductDetailSlugView(ObjectViewedMixin, DetailView):
+class ProductDetailSlugView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
-        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
-        context['cart'] = cart_obj
-        return context
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
+    #     cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+    #     context['cart'] = cart_obj
+    #     return context
 
     def get_object(self, *args, **kwargs):
         request = self.request
@@ -54,3 +54,49 @@ class ProductDetailSlugView(ObjectViewedMixin, DetailView):
         except:
             raise Http404("Uhhmmm ")
         return instance
+class ProductDownloadView(View):
+    def get(self, request, *args, **kwargs):
+        slug = kwargs.get('slug')
+        pk = kwargs.get('pk')
+        downloads_qs = ProductFile.objects.filter(pk=pk, product__slug=slug)
+        if downloads_qs.count() != 1:
+            raise Http404("Download not found")
+        download_obj = downloads_qs.first()
+        # permission checks
+
+        can_download = False
+        user_ready  = True
+        if download_obj.user_required:
+            if not request.user.is_authenticated():
+                user_ready = False
+
+        purchased_products = Product.objects.none()
+        if download_obj.free:
+            can_download = True
+            user_ready = True
+        else:
+            # not free
+            purchased_products = ProductPurchase.objects.products_by_request(request)
+            if download_obj.product in purchased_products:
+                can_download = True
+        if not can_download or not user_ready:
+            messages.error(request, "You do not have access to download this item")
+            return redirect(download_obj.get_default_url())
+
+        aws_filepath = download_obj.generate_download_url()
+        print(aws_filepath)
+        return HttpResponseRedirect(aws_filepath)
+        # file_root = settings.PROTECTED_ROOT
+        # filepath = download_obj.file.path # .url /media/
+        # final_filepath = os.path.join(file_root, filepath) # where the file is stored
+        # with open(final_filepath, 'rb') as f:
+        #     wrapper = FileWrapper(f)
+        #     mimetype = 'application/force-download'
+        #     gussed_mimetype = guess_type(filepath)[0] # filename.mp4
+        #     if gussed_mimetype:
+        #         mimetype = gussed_mimetype
+        #     response = HttpResponse(wrapper, content_type=mimetype)
+        #     response['Content-Disposition'] = "attachment;filename=%s" %(download_obj.name)
+        #     response["X-SendFile"] = str(download_obj.name)
+        #     return response
+        #return redirect(download_obj.get_default_url())
