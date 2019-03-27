e33161c4839a93e6a21285faaf8e94d0bd9da2ae
Gautam <Satadhi@Gautams-MacBook-Air.local>
2019-03-27 08:20:39 +0530

    hehe

--- src/ecommerce/products/views.py
+++ src/ecommerce/products/views.py
@@ -29,28 +29,28 @@ def product_detail_view(request,pk=None , *args, **kwargs):
     }
     return render(request, "products/detail.html",context)
 
-# class ProductDetailSlugView(ObjectViewedMixin, DetailView):
-#     queryset = Product.objects.all()
-#     template_name = "products/detail.html"
-#
-#     def get_context_data(self, *args, **kwargs):
-#         context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
-#         cart_obj, new_obj = Cart.objects.new_or_get(self.request)
-#         context['cart'] = cart_obj
-#         return context
-#
-#     def get_object(self, *args, **kwargs):
-#         request = self.request
-#         slug = self.kwargs.get('slug')
-#
-#         #instance = get_object_or_404(Product, slug=slug, active=True)
-#         try:
-#             instance = Product.objects.get(slug=slug, active=True)
-#         except Product.DoesNotExist:
-#             raise Http404("Not found..")
-#         except Product.MultipleObjectsReturned:
-#             qs = Product.objects.filter(slug=slug, active=True)
-#             instance = qs.first()
-#         except:
-#             raise Http404("Uhhmmm ")
-#         return instance
+class ProductDetailSlugView(ObjectViewedMixin, DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+
+        #instance = get_object_or_404(Product, slug=slug, active=True)
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found..")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Uhhmmm ")
+        return instance
